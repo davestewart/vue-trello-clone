8e9803f9d5935406f8ed0909ea8d4d3c40851d40
Dave Stewart <dave@davestewart.co.uk>
2018-06-11 21:02:07 +0100

    Removed router and vuex

--- src/app/main.js
+++ src/app/main.js
@@ -3,8 +3,6 @@ import VeeValidate from 'vee-validate'
 import 'bulma'
 
 import App from './App.vue'
-import router from './router'
-import store from './state'
 
 import 'styles/index.scss'
 import 'core/ui'
@@ -17,13 +15,10 @@ Vue.use(VeeValidate)
 
 // application
 const app = new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
 
 // debugging
 if (process.env.NODE_ENV === 'development') {
   window.app = app
-  window.store = store
 }
