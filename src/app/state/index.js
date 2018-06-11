dc8e3e8a9a9316196b88530a0c0e8cbdaec78a44
Dave Stewart <dave@davestewart.co.uk>
2018-06-12 00:30:56 +0100

    Added persisted state

--- src/app/state/index.js
+++ src/app/state/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import persistedState from 'vuex-persistedstate';
 
 import board from './board'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [
+    persistedState()
+  ],
   modules: {
     board
   }
